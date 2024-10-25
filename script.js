6ca26ccf0889b25cbf6d51c1c1f3c8caf2242ea6
raghadriad <raghadriad035@gmail.com>
2024-10-25 18:42:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function displayMessage() {
+    const message = document.getElementById("welcome-message");
+    message.textContent = "شكرًا لزيارتك! نتمنى أن تستمتع بمحتوى موقعنا.";
+    message.style.color = "#4CAF50";
+  }
